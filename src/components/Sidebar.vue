f352a0c0cc3907bae96f3399746af1b6fc001751
IzatheGreat15 <20100869@usc.edu.ph>
2024-03-17 22:52:13 +0800

    Add bg to menu button

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,7 +5,7 @@
             Logout
           </div>
           <div class="text-right">
-            <svg xmlns="http://www.w3.org/2000/svg" @click="$emit('toggleSidebar')" width="24" height="24" fill="currentColor" class="bi bi-list text-center" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" @click="$emit('toggleSidebar')" width="24" height="24" fill="currentColor" class="bi bi-list text-center bg-secondary rounded" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
             </svg>
           </div>
